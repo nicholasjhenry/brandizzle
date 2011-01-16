27d3805ac1310156573cc7fa883e3ec876fa162c
Nicholas Henry <nicholas.henry@gmail.com>
2011-01-16 13:06:49 -0500

    Remove duplicate code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   def show
-    if %w(about).include?(params[:id])
-      render "pages/show/#{params[:id]}"
+    page_id = params[:id]
+    if %w(about).include?(page_id)
+      render "pages/show/#{page_id}"
     else
       render :file => 'public/404.html', :status => 404
     end
