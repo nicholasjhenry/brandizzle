c989f9c8ad141bc91b25cf70926adeddba0f5afc
Nicholas Henry <nicholas.henry@gmail.com>
2011-01-16 10:21:50 -0500

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandizzle::Application.config.secret_token = 'a7112624435ddde711f723d6c991cef0c9d46d13806c12c319b6291146c7b33a602c356b53fa7b00a24c260479ddf1c0199d744d7c983c605fa16d83cd7a10c7'
