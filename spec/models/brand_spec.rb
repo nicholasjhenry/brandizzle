d5c874a33b4ba7d50c511707a6efbed5a2aa3a37
Nicholas Henry <nicholas.henry@gmail.com>
2011-08-06 16:03:53 -0400

    Assign subject to brand spec

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -4,20 +4,22 @@ describe Brand do
   it { should validate_presence_of(:name) }
   it { should have_and_belong_to_many(:searches) }
 
+  subject { Factory(:brand) }
+
   describe "#add_search" do
     let (:brand) { Factory(:brand) }
 
     context "when search does not exist" do
       it "creates a new search" do
         lambda {
-          brand.add_search('foo')
+          subject.add_search('foo')
         }.should change(Search, :count)
       end
 
       it "associates the search with brand" do
-        brand.add_search('foo')
+        subject.add_search('foo')
 
-        brand.searches.map(&:term).should include('foo')
+        subject.searches.map(&:term).should include('foo')
       end
     end
 
@@ -28,30 +30,29 @@ describe Brand do
 
       it "should not create a new search" do
         lambda {
-          brand.add_search('foo')
+          subject.add_search('foo')
         }.should_not change(Search, :count)
       end
 
       it "associates it with the brand" do
-        brand.add_search('foo')
+        subject.add_search('foo')
 
-        brand.searches.map(&:term).should include('foo')
+        subject.searches.map(&:term).should include('foo')
       end
     end
   end
 
   describe "#remove_search" do
-    let (:brand) { Factory(:brand) }
     let (:another_brand) { Factory(:brand) }
 
     before do
-      @bar_search =  brand.add_search('bar')
+      @bar_search =  subject.add_search('bar')
     end
     
     context "when a search exists is not associated with another brand" do
       it "removes the search from the brand" do
-        brand.remove_search(@bar_search)
-        brand.searches.map(&:term).should_not include('bar')
+        subject.remove_search(@bar_search)
+        subject.searches.map(&:term).should_not include('bar')
       end
     end
 
@@ -62,7 +63,7 @@ describe Brand do
 
       it "does not destroy the search" do
         lambda {
-          brand.remove_search(@bar_search)
+          subject.remove_search(@bar_search)
         }.should_not change(Search, :count)
       end
     end
@@ -74,7 +75,7 @@ describe Brand do
 
       it "does nothing" do
         lambda {
-          brand.remove_search(@foo_search)
+          subject.remove_search(@foo_search)
         }.should_not change(Search, :count)
       end
     end
