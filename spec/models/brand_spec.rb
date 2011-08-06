bb84182d696c07522e0bb2f89463230297ba55e6
Nicholas Henry <nicholas.henry@gmail.com>
2011-08-06 16:00:20 -0400

    Extract contexts from assertions

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -7,56 +7,76 @@ describe Brand do
   describe "#add_search" do
     let (:brand) { Factory(:brand) }
 
-    it "creates a new search associated if it does not exist yet" do
-      lambda {
+    context "when search does not exist" do
+      it "creates a new search" do
+        lambda {
+          brand.add_search('foo')
+        }.should change(Search, :count)
+      end
+
+      it "associates the search with brand" do
         brand.add_search('foo')
-      }.should change(Search, :count)
-      brand.searches.map(&:term).should include('foo')
+
+        brand.searches.map(&:term).should include('foo')
+      end
     end
 
-    it "for an existing search it associates it with the brand and does not create a new search" do
-      Factory(:search, :term => :foo)
+    context "with an existing search" do
+      before do
+        Factory(:search, :term => :foo)
+      end
 
-      lambda {
+      it "should not create a new search" do
+        lambda {
+          brand.add_search('foo')
+        }.should_not change(Search, :count)
+      end
+
+      it "associates it with the brand" do
         brand.add_search('foo')
-      }.should_not change(Search, :count)
-      brand.searches.map(&:term).should include('foo')
+
+        brand.searches.map(&:term).should include('foo')
+      end
     end
   end
 
   describe "#remove_search" do
     let (:brand) { Factory(:brand) }
+    let (:another_brand) { Factory(:brand) }
 
     before do
       @bar_search =  brand.add_search('bar')
     end
     
-    it "removes the search from the brand" do
-      brand.remove_search(@bar_search)
-      brand.searches.map(&:term).should_not include('bar')
-    end
-
-    it "does not destroy the search if it is associated to any brand" do
-      another_brand = Factory(:brand)
-      search = another_brand.add_search('bar')
-      lambda {
-        brand.remove_search(search)
-      }.should_not change(Search, :count)
-    end
-
-    it "destroys the search if it is no longer associated to any brand" do
-      lambda {
+    context "when a search exists is not associated with another brand" do
+      it "removes the search from the brand" do
         brand.remove_search(@bar_search)
-      }.should change(Search, :count)
-      Search.find_by_term('bar').should be_nil
+        brand.searches.map(&:term).should_not include('bar')
+      end
     end
 
-    it "does nothing for a search not associated with the brand" do
-      another_brand = Factory(:brand)
-      search = another_brand.add_search('foo')
-      lambda {
-        brand.remove_search(search)
-      }.should_not change(Search, :count)
+    context "when a search term is associated with another brand" do
+      before do
+        @another_bar_search = another_brand.add_search('bar')
+      end
+
+      it "does not destroy the search" do
+        lambda {
+          brand.remove_search(@bar_search)
+        }.should_not change(Search, :count)
+      end
+    end
+
+    context "when search term is not associated with the brand" do
+      before do
+        @foo_search = another_brand.add_search('foo')
+      end
+
+      it "does nothing" do
+        lambda {
+          brand.remove_search(@foo_search)
+        }.should_not change(Search, :count)
+      end
     end
   end
 end
